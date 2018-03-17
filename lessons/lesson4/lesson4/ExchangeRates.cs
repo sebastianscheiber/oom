df5c3083949ff64ee61af6b0903112ee146672e5
Stefan Maierhofer <sm@stefanmaierhofer.com>
2018-03-17 09:34:36 +0100

    version updates

--- lessons/lesson4/lesson4/ExchangeRates.cs
+++ lessons/lesson4/lesson4/ExchangeRates.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Net;
@@ -16,22 +17,23 @@ namespace lesson4
         {
             // exchange rate is 1:1 for same currency
             if (from == to) return 1;
-            
+
             // use web service to query current exchange rate
             // request : http://download.finance.yahoo.com/d/quotes.csv?s=EURUSD=X&f=sl1d1t1c1ohgv&e=.csv
             // response: "EURUSD=X",1.0930,"12/29/2015","6:06pm",-0.0043,1.0971,1.0995,1.0899,0
-            var key = string.Format("{0}{1}", from, to); // e.g. EURUSD means "How much is 1 EUR in USD?".
-            // if we've already downloaded this exchange rate, use the cached value
-            if (s_rates.ContainsKey(key)) return s_rates[key];
+            var key = string.Format("{0}{1}", from.ToString(), to.ToString()); // e.g. EURUSD means "How much is 1 EUR in USD?".
 
-            // otherwise create the request URL, ...
-            var url = string.Format(@"http://download.finance.yahoo.com/d/quotes.csv?s={0}=X&f=sl1d1t1c1ohgv&e=.csv", key);
+            // use web service to query current exchange rate
+            // request : https://api.fixer.io/latest?base=EUR&symbols=USD
+            // response: {"base":"EUR","date":"2018-01-24","rates":{"USD":1.2352}}
+            var url = $"https://api.fixer.io/latest?base={from}&symbols={to}";
             // download the response as string
             var data = new WebClient().DownloadString(url);
-            // split the string at ','
-            var parts = data.Split(',');
+            // parse JSON
+            var json = JObject.Parse(data);
             // convert the exchange rate part to a decimal 
-            var rate = decimal.Parse(parts[1], CultureInfo.InvariantCulture);
+            var rate = decimal.Parse((string)json["rates"][to.ToString()], CultureInfo.InvariantCulture);
+
             // cache the exchange rate
             s_rates[key] = rate;
 
